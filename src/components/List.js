d6a648536e1e3094a937a07191619b8e92755cf9
leeymk <leeymk11@gmail.com>
2023-01-19 07:31:15 +0900

    tailwind_fin

--- src/components/List.js
+++ src/components/List.js
@@ -23,16 +23,21 @@ export default function List({ todoData, setTodoData }) {
   return (
     <div>
       {todoData.map((data) => (
-          <div  key={data.id}>
-            <div className="flex items-center justfi-between w-full px-4 py-1 my-2 text-gray-600">
-              <input 
-                type="checkbox" 
-                defaultChecked={false} 
-                onChange={() => handleCompleteChange(data.id)} />
-              {data.title}
-              <button onClick={() => handleClick(data.id)}>
-                X
-              </button>
+          <div key={data.id}>
+            <div className="flex items-center justfy-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded">
+              <div className="items-center">
+                <input 
+                  type="checkbox" 
+                  defaultChecked={false} 
+                  onChange={() => handleCompleteChange(data.id)} />{" "}
+                <span className={data.completed? 'line-through' : undefined }>{data.title}</span>
+              </div>
+              <div className="items-center">
+                <button className="px-4 py-2 float-right" onClick={() => handleClick(data.id)}>
+                  X
+                </button>
+              </div>
+            </div>
           </div>
       ))}
     </div>
